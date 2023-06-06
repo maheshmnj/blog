6693689d280146fa443d49c41186352d66c6c221
mahesh jamdade <maheshmn121@gmail.com>
2023-06-05 22:41:26 -0400

    update workflow

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
           },
           { to: '/blog', label: 'Blog', position: 'left' },
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/maheshmnj/blog',
             label: 'GitHub',
             position: 'right',
           },
