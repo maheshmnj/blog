dd973d3019ceb8ae946c702dab79b4760e61ba5d
mahesh jamdade <maheshmn121@gmail.com>
2023-10-06 18:31:15 -0400

    add blog on genetic algorithm

---
+++ src/components/blog/Headline.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Headline(props) {
+  // Created a text with alignment start, center, end
+  const align = props.align ? props.align : "start";
+  const text = props.text ? props.text : "Headline";
+  const color = props.color ? props.color : "black";
+  const size = props.size ? props.size : "1.5rem";
+  const weight = props.weight ? props.weight : "bold";
+  const style = props.style ? props.style : "normal";
+
+  return (
+    <div
+      style={{
+        textAlign: align,
+        color: color,
+        fontSize: size,
+        fontWeight: weight,
+        fontStyle: style,
+      }}
+    >
+      {text}
+    </div>
+  );
+}
+
+export default Headline;
